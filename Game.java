4e4c084cd4fae94247a13fa4fc406341103e1703
Sumaiyya B. <sumaiyya.baig@jpschools.org>
2026-03-05 11:02:35 -0600

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,3 +1,21 @@
 public class Game {
+    private String name; 
+    private int happinessIncr; 
+    private int weightDecr; 
+    
+    public Game(String name, int happinessIncr, int weightDecr){
+        this.name = name;
+        this.happinessIncr = happinessIncr;
+        this.weightDecr = weightDecr;
+    }
+    public String getName(){
+        return this.name; 
+    }
+    public int getHappinessIncr(){
+        return this.happinessIncr; 
+    }
+    public int getWeightDecr(){
+        return this.weightDecr; 
+    }
     
 }
